6dfd5fbff3be64751d1c30de83bc7dfc79abbe80
Arkadii Ivanov <arkann1985@gmail.com>
2024-02-03 21:45:33 +0000

    Show UI on desktop app

--- composeApp/src/commonMain/kotlin/com/arkivanov/minesweeper/App.kt
+++ composeApp/src/commonMain/kotlin/com/arkivanov/minesweeper/App.kt
@@ -1,12 +1,15 @@
 package com.arkivanov.minesweeper
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.arkivanov.minesweeper.game.GameComponent
+import com.arkivanov.minesweeper.game.GameContent
 
 @Composable
-internal fun App() {
+internal fun App(component: GameComponent) {
     MaterialTheme {
-        Text("Hello Minesweeper")
+        GameContent(component = component, modifier = Modifier.fillMaxSize())
     }
 }
--- composeApp/src/jvmMain/kotlin/Main.kt
+++ composeApp/src/jvmMain/kotlin/Main.kt
@@ -1,11 +1,31 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.lifecycle.LifecycleController
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.arkivanov.minesweeper.App
+import com.arkivanov.minesweeper.game.DefaultGameComponent
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
 
 fun main() {
+    val lifecycle = LifecycleRegistry()
+
+    val root =
+        DefaultGameComponent(
+            componentContext = DefaultComponentContext(lifecycle = lifecycle),
+            storeFactory = DefaultStoreFactory(),
+        )
+
     application {
-        Window(onCloseRequest = ::exitApplication, title = "Minesweeper") {
-            App()
+        val windowState = rememberWindowState()
+
+        Window(onCloseRequest = ::exitApplication, title = "Minesweeper", state = windowState) {
+            App(component = root)
         }
+
+        @OptIn(ExperimentalDecomposeApi::class)
+        LifecycleController(lifecycle, windowState)
     }
 }
