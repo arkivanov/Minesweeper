956aed51ee92f954c60615ff8a770846979d8570
Arkadii Ivanov <arkann1985@gmail.com>
2024-02-04 20:27:16 +0000

    Run Wasm browser app

--- composeApp/src/wasmJsMain/kotlin/Main.kt
+++ composeApp/src/wasmJsMain/kotlin/Main.kt
@@ -1,8 +1,23 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.essenty.lifecycle.resume
 import com.arkivanov.minesweeper.App
+import com.arkivanov.minesweeper.game.DefaultGameComponent
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    CanvasBasedWindow(canvasElementId = "ComposeTarget") { App() }
+    val lifecycle = LifecycleRegistry()
+
+    val root =
+        DefaultGameComponent(
+            componentContext = DefaultComponentContext(lifecycle = lifecycle),
+            storeFactory = DefaultStoreFactory(),
+        )
+
+    lifecycle.resume()
+
+    CanvasBasedWindow(canvasElementId = "ComposeTarget") { App(root ) }
 }
