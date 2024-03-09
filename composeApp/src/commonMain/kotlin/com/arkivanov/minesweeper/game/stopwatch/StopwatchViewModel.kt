a1861eb66f9e0dbc5a3593c70efe906641fab92e
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-03-09 19:06:28 +0300

    using coroutineContext.isActive instead of NonCancellable.isActive - for prevent failing build

--- composeApp/src/commonMain/kotlin/com/arkivanov/minesweeper/game/stopwatch/StopwatchViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/arkivanov/minesweeper/game/stopwatch/StopwatchViewModel.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.NonCancellable.isActive
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
