5fe3607e4ea1a8ca53bedf9ffa40cfe572d0d1f0
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-03-13 10:04:19 +0300

    1) Changes given from code review 2) shift timer to different values

--- composeApp/src/commonTest/kotlin/com/arkivanov/minesweeper/game/DefaultGameComponentTest.kt
+++ composeApp/src/commonTest/kotlin/com/arkivanov/minesweeper/game/DefaultGameComponentTest.kt
@@ -18,7 +18,7 @@ class DefaultGameComponentTest {
     private val gameComponent = DefaultGameComponent(
         componentContext = DefaultComponentContext(lifecycle = lifecycle),
         storeFactory = DefaultStoreFactory(),
-        settings = GameSettings(width = 20, height = 20, maxMines = 398),
+        settings = GameSettings(width = 20, height = 20, maxMines = 200),
         mainCoroutineContext = StandardTestDispatcher(scheduler = coroutineScheduler)
     )
 
@@ -32,21 +32,21 @@ class DefaultGameComponentTest {
 
     @Test
     fun GIVEN_created_WHEN_cell_clicked_THEN_status_STARTED() {
-        clickCellPrimary(x = 0, y = 0)
+        clickCellPrimary(0 by 0)
 
         assertEquals(GameStatus.STARTED, state.gameStatus)
     }
 
     @Test
     fun GIVEN_created_WHEN_cell_clicked_THEN_timer_shows_0() {
-        clickCellPrimary(x = 0, y = 0)
+        clickCellPrimary(0 by 0)
 
         assertEquals(0, state.timer)
     }
 
     @Test
     fun GIVEN_game_started_WHEN_one_second_passed_THEN_timer_shows_1() {
-        clickCellPrimary(x = 0, y = 0)
+        clickCellPrimary(0 by 0)
 
         coroutineScheduler.advanceTimeBy(1.seconds)
         coroutineScheduler.runCurrent()
@@ -56,21 +56,39 @@ class DefaultGameComponentTest {
 
     @Test
     fun GIVEN_game_started_WHEN_click_on_mine_cell_THEN_timer_STOPS() {
-        clickCellPrimary(x = 0, y = 0)
+        clickCellPrimary(0 by 0)
 
         coroutineScheduler.advanceTimeBy(10.seconds)
         coroutineScheduler.runCurrent()
 
-        clickCellPrimary(x = 10, y = 10)
+        val (mineLocation) = state.grid.entries.first { (_, cell) -> cell.value.isMine }
+        clickCellPrimary(mineLocation)
 
-        coroutineScheduler.advanceTimeBy(10.seconds)
+        coroutineScheduler.advanceTimeBy(20.seconds)
         coroutineScheduler.runCurrent()
 
         assertEquals(10, state.timer)
     }
 
-    private fun clickCellPrimary(x: Int, y: Int) {
-        gameComponent.onCellTouchedPrimary(x = x, y = y)
-        gameComponent.onCellReleased(x = y, y = y)
+    @Test
+    fun GIVEN_game_started_WHEN_status_win_THEN_timer_STOPS() {
+        clickCellPrimary(0 by 0)
+
+        coroutineScheduler.advanceTimeBy(25.seconds)
+        coroutineScheduler.runCurrent()
+
+        state.grid.filterValues { it.status.isClosed && !it.value.isMine }.forEach { (location, _) ->
+            clickCellPrimary(location)
+        }
+
+        coroutineScheduler.advanceTimeBy(50.seconds)
+        coroutineScheduler.runCurrent()
+
+        assertEquals(25, state.timer)
+    }
+
+    private fun clickCellPrimary(location: Location) {
+        gameComponent.onCellTouchedPrimary(x = location.x, y = location.y)
+        gameComponent.onCellReleased(x = location.x, y = location.y)
     }
 }
