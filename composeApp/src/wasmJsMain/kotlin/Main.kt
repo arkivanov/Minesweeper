fcc905bd63b3763ea8197d5691e74c479495c4a8
Arkadii Ivanov <arkann1985@gmail.com>
2024-04-15 19:23:54 +0100

    Control LifecycleRegistry on Wasm

--- composeApp/src/wasmJsMain/kotlin/Main.kt
+++ composeApp/src/wasmJsMain/kotlin/Main.kt
@@ -3,14 +3,17 @@ import androidx.compose.ui.window.CanvasBasedWindow
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.arkivanov.essenty.lifecycle.resume
+import com.arkivanov.essenty.lifecycle.stop
 import com.arkivanov.essenty.statekeeper.StateKeeperDispatcher
 import com.arkivanov.minesweeper.decodeSerializableContainer
 import com.arkivanov.minesweeper.encodeToString
 import com.arkivanov.minesweeper.root.DefaultRootComponent
 import com.arkivanov.minesweeper.root.RootContent
 import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
+import kotlinx.browser.document
 import kotlinx.browser.localStorage
 import kotlinx.browser.window
+import org.w3c.dom.Document
 import org.w3c.dom.get
 import org.w3c.dom.set
 
@@ -25,7 +28,7 @@ fun main() {
             storeFactory = DefaultStoreFactory(),
         )
 
-    lifecycle.resume()
+    lifecycle.attachToDocument()
 
     window.onbeforeunload =
         {
@@ -40,3 +43,21 @@ fun main() {
 }
 
 private const val KEY_SAVED_STATE = "saved_state"
+
+private fun LifecycleRegistry.attachToDocument() {
+    fun onVisibilityChanged() {
+        if (visibilityState(document) == "visible") {
+            resume()
+        } else {
+            stop()
+        }
+    }
+
+    onVisibilityChanged()
+
+    document.addEventListener(type = "visibilitychange", callback = { onVisibilityChanged() })
+}
+
+// Workaround for Document#visibilityState not available in Wasm
+@JsFun("(document) => document.visibilityState")
+private external fun visibilityState(document: Document): String
